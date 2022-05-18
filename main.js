20bda2132c9f462488d81a3fb4cd0744e3256b27
Deepdeeps <82767467+Deepdeeps@users.noreply.github.com>
2022-05-18 11:32:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+function preload() {
+	mario_gameover = loadSound("gameover.wav");//143 till below 
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+
+	//C139
+	world_start = loadSound("world_start.wav"); //background music
+	setSprites();//load all the images used in the game which is already written, no need to worry abt this
+	MarioAnimation();//function will loads all the animation required for the Mario game
+}
+//Below code for creating canvas
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+
+	instializeInSetup(mario);//- this line will set up all the 	required variables and functions needed for mario game,
+	
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');  //parent() is a p5.js function used to put canvas or any p5.js component like webcam live 	view inside a HTML div.
+	
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	}
+  }
+   //draw() being called several times in a second
+function draw() {
+	game(); //- this function will start the Mario game, already written
+}
+
+
+
+
+
+
